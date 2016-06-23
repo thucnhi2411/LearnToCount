f46a0c751878c10037c7e0e28b66a8c5a2ee3294
thucnhi2411 <let@lafayette.edu>
2016-06-23 16:28:20 +0700

    Initial Commit

---
+++ LearnToCount/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnToCount
+//
+//  Created by THUC NHI on 6/23/16.
+//  Copyright © 2016 THUC-NHI LE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
